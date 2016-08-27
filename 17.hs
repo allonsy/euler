178e1bac30dabb68b04d8e2ef02bc5f7965db7e6
Alec Snyder <linuxbash8@gmail.com>
2016-08-26 17:14:58 -0700

    add seventeen

---
+++ 17.hs
@@ -0,0 +1,43 @@
+module Main where
+
+
+numberToString :: Int -> String
+numberToString 1 = "one"
+numberToString 2 = "two"
+numberToString 3 = "three"
+numberToString 4 = "four"
+numberToString 5 = "five"
+numberToString 6 = "six"
+numberToString 7 = "seven"
+numberToString 8 = "eight"
+numberToString 9 = "nine"
+numberToString 10 = "ten"
+numberToString 11 = "eleven"
+numberToString 12 = "twelve"
+numberToString 13 = "thirteen"
+numberToString 14 = "fourteen"
+numberToString 15 = "fifteen"
+numberToString 16 = "sixteen"
+numberToString 17 = "seventeen"
+numberToString 18 = "eighteen"
+numberToString 19 = "nineteen"
+numberToString 20 = "twenty"
+numberToString 30 = "thirty"
+numberToString 40 = "fourty"
+numberToString 50 = "fifty"
+numberToString 60 = "sixty"
+numberToString 70 = "seventy"
+numberToString 80 = "eighty"
+numberToString 90 = "ninety"
+numberToString 100 = "hundred"
+numberToString 1000 = "thousand"
+
+breakUpNumber :: Int -> [Int]
+breakUpNumber =
+  strRep = map (\c -> [c]) $ show breakUpNumber
+
+
+
+main :: IO ()
+main = do
+  print $ "hello"
